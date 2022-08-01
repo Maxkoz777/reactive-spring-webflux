d4c4907580f012c5ff297eaf8e0e3e19ebc55e86
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-08-01 21:03:29 +0300

    FluxAndMonoGeneratorService filled with example of flux subscription

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,21 @@
 package com.learnreactiveprogramming.service;
 
+import java.util.List;
+import reactor.core.publisher.Flux;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        // This flux can be created from DB or remote service call
+        return Flux.fromIterable(List.of("Max", "Alex", "Nick"));
+    }
+
+    public static void main(String[] args) {
+
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux()
+            .subscribe(name -> System.out.println("Name is " + name ));
+
+    }
+
 }
