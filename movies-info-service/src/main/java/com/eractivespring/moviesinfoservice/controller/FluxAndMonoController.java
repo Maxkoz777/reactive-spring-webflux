59b76c803c71e4a002c23b0e946c80864f7ab97c
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-08-03 19:17:16 +0300

    FluxAndMonoController added with non-blocking SSE infinite stream api example

--- movies-info-service/src/main/java/com/eractivespring/moviesinfoservice/controller/FluxAndMonoController.java
+++ movies-info-service/src/main/java/com/eractivespring/moviesinfoservice/controller/FluxAndMonoController.java
@@ -1,5 +1,7 @@
 package com.eractivespring.moviesinfoservice.controller;
 
+import java.time.Duration;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -16,6 +18,11 @@ public class FluxAndMonoController {
     @GetMapping("/mono")
     public Mono<String> mono() {
         return Mono.just("Hello World!").log();
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> stream() {
+        return Flux.interval(Duration.ofSeconds(1)).log();
     }
 
 }
