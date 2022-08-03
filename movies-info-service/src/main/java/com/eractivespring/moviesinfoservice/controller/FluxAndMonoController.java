7f770cc0558f6ab8f3afc0ac46f6fbee286cd74c
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-08-03 19:12:27 +0300

    FluxAndMonoController added with non-blocking mono api example

--- movies-info-service/src/main/java/com/eractivespring/moviesinfoservice/controller/FluxAndMonoController.java
+++ movies-info-service/src/main/java/com/eractivespring/moviesinfoservice/controller/FluxAndMonoController.java
@@ -3,6 +3,7 @@ package com.eractivespring.moviesinfoservice.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class FluxAndMonoController {
@@ -10,6 +11,11 @@ public class FluxAndMonoController {
     @GetMapping("/flux")
     public Flux<Integer> flux() {
         return Flux.just(1, 2, 3).log();
+    }
+
+    @GetMapping("/mono")
+    public Mono<String> mono() {
+        return Mono.just("Hello World!").log();
     }
 
 }
