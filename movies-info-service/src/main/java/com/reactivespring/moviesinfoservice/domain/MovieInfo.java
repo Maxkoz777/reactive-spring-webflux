9bed946d4db77e93ad430bc2574b315574fefe16
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-08-04 14:55:58 +0300

    MovieInfo model added

---
+++ movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/domain/MovieInfo.java
@@ -0,0 +1,23 @@
+package com.reactivespring.moviesinfoservice.domain;
+
+import java.time.LocalDate;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieInfo {
+
+    private String movieInfoId;
+    private String name;
+    private Integer year;
+    private List<String> cast;
+    private LocalDate releaseDate;
+
+
+}
