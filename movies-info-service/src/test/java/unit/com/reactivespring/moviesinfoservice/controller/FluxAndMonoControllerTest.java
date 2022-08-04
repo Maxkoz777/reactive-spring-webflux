f889714a218e30b1778774c4ebb206130bf14028
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-08-04 14:42:45 +0300

    FluxAndMonoControllerTest added with various test types for /flux endpoint

--- movies-info-service/src/test/java/unit/com/reactivespring/moviesinfoservice/controller/FluxAndMonoControllerTest.java
+++ movies-info-service/src/test/java/unit/com/reactivespring/moviesinfoservice/controller/FluxAndMonoControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
 
 @AutoConfigureWebTestClient
 @WebFluxTest(controllers = FluxAndMonoController.class)
@@ -16,9 +17,33 @@ class FluxAndMonoControllerTest {
     WebTestClient webTestClient;
 
     @Test
-    void flux() {
+    void fluxListSizeTest() {
         webTestClient.get().uri("/flux").exchange()
             .expectStatus().is2xxSuccessful()
             .expectBodyList(Integer.class).hasSize(3);
     }
+
+    @Test
+    void fluxListElementsTest() {
+        var flux = webTestClient.get().uri("/flux").exchange()
+            .expectStatus().is2xxSuccessful()
+            .returnResult(Integer.class).getResponseBody();
+
+        StepVerifier.create(flux)
+            .expectNext(1, 2, 3)
+            .verifyComplete();
+    }
+
+    @Test
+    void fluxListExchangeElementsTest() {
+        webTestClient.get().uri("/flux").exchange()
+            .expectStatus().is2xxSuccessful()
+            .expectBodyList(Integer.class)
+            .consumeWith(listEntityExchangeResult -> {
+                var responseBody = listEntityExchangeResult.getResponseBody();
+                assertNotNull(responseBody);
+                assertEquals(3, responseBody.size());
+            });
+    }
+
 }
