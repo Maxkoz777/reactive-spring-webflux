831d28b1e7044c79edfa08d419c7eb135c02f233
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-08-06 14:43:16 +0300

    MovieInfoController added with POST movieInfo endpoint

--- movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/controller/MovieInfoController.java
+++ movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/controller/MovieInfoController.java
@@ -1,9 +1,16 @@
 package com.reactivespring.moviesinfoservice.controller;
 
+import com.reactivespring.moviesinfoservice.domain.dto.MovieInfoDto;
+import com.reactivespring.moviesinfoservice.domain.entity.MovieInfo;
 import com.reactivespring.moviesinfoservice.service.MovieInfoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/v1")
@@ -11,5 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieInfoController {
 
     private final MovieInfoService movieInfoService;
+
+    @PostMapping("/movieInfo")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfoDto movieInfoDto) {
+        return movieInfoService.addMovieInfo(movieInfoDto);
+    }
+
+//    @GetMapping("/movieInfo")
 
 }
