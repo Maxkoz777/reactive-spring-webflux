3e490b95ca03997373b53ff976d62e426b0bcf97
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-08-06 18:32:07 +0300

    MovieInfoMapper added with entity update method

--- movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/domain/mapper/MovieInfoMapper.java
+++ movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/domain/mapper/MovieInfoMapper.java
@@ -2,7 +2,10 @@ package com.reactivespring.moviesinfoservice.domain.mapper;
 
 import com.reactivespring.moviesinfoservice.domain.dto.MovieInfoDto;
 import com.reactivespring.moviesinfoservice.domain.entity.MovieInfo;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper
 public interface MovieInfoMapper {
@@ -10,5 +13,8 @@ public interface MovieInfoMapper {
     MovieInfo movieInfoDtoToMovieInfo(MovieInfoDto movieInfoDto);
 
     MovieInfoDto movieInfoToMovieInfoDto(MovieInfo movieInfo);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateMovieInfoFromDto(MovieInfoDto dto, @MappingTarget MovieInfo movieInfo);
 
 }
