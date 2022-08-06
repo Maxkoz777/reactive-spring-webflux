f597c857db47c013e943738c570f0ce17a397356
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-08-06 18:38:55 +0300

    MovieInfoController added with delete and update endpoints

--- movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/controller/MovieInfoController.java
+++ movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/controller/MovieInfoController.java
@@ -5,9 +5,11 @@ import com.reactivespring.moviesinfoservice.domain.entity.MovieInfo;
 import com.reactivespring.moviesinfoservice.service.MovieInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,26 +18,41 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/v1/movieInfo")
 @RequiredArgsConstructor
 public class MovieInfoController {
 
     private final MovieInfoService movieInfoService;
 
-    @PostMapping("/movieInfo")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfoDto movieInfoDto) {
         return movieInfoService.addMovieInfo(movieInfoDto);
     }
 
-    @GetMapping("/movieInfo")
+    @GetMapping
     public Flux<MovieInfo> getAllMovieInfos() {
         return movieInfoService.getAllMovieInfos();
     }
 
-    @GetMapping("/movieInfo/{id}")
+    @GetMapping("/{id}")
     public Mono<MovieInfo> getMovieInfoById(@PathVariable String id) {
         return movieInfoService.getMovieInfoById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<MovieInfo> updateMovieInfoForId(@RequestBody MovieInfoDto movieInfoDto, @PathVariable String id) {
+        return movieInfoService.updateMovieInfo(id, movieInfoDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deleteMovieInfoForId(@PathVariable String id) {
+        return movieInfoService.deleteMovieInfoById(id);
+    }
+
+    @DeleteMapping
+    public Mono<Void> deleteAllMovieInfos() {
+        return movieInfoService.deleteAll();
     }
 
 }
