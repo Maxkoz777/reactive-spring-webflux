bf85886b37e267463a99a9420c897ec4566fd71c
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-08-08 13:09:54 +0300

    MovieInfoDto added with constraints

--- movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/domain/dto/MovieInfoDto.java
+++ movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/domain/dto/MovieInfoDto.java
@@ -2,6 +2,11 @@ package com.reactivespring.moviesinfoservice.domain.dto;
 
 import java.time.LocalDate;
 import java.util.List;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +15,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MovieInfoDto {
+    @NotBlank
     private String name;
+    @NotNull
+    @Positive
     private Integer year;
+    @Size(min = 1)
     private List<String> cast;
+    @PastOrPresent
     private LocalDate releaseDate;
 }
