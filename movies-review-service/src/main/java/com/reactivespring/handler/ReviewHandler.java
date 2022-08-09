5eeb5306d3e1cf8bc4e7d6e43f4a8d9fabf429ed
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-08-10 01:43:40 +0300

    ReviewHandler added with save() method handler

--- movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
+++ movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
@@ -1,4 +1,27 @@
 package com.reactivespring.handler;
 
+import com.reactivespring.domain.Review;
+import com.reactivespring.repository.ReviewReactiveRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
 public class ReviewHandler {
+
+    private final ReviewReactiveRepository repository;
+
+    public Mono<ServerResponse> addReview(ServerRequest request) {
+
+        return request.bodyToMono(Review.class)
+            .flatMap(repository::save)
+            .flatMap(ServerResponse.status(HttpStatus.CREATED)::bodyValue)
+            .log();
+
+    }
+
 }
