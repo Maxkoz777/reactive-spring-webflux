7553755fc9b4c0a2f0900f1befecd74f570dd0b9
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-08-09 21:39:06 +0300

    MovieInfoRepository added with ability to search entities by provided year

--- movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/repository/MovieInfoRepository.java
+++ movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/repository/MovieInfoRepository.java
@@ -2,6 +2,10 @@ package com.reactivespring.moviesinfoservice.repository;
 
 import com.reactivespring.moviesinfoservice.domain.entity.MovieInfo;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 public interface MovieInfoRepository extends ReactiveMongoRepository<MovieInfo, String> {
+
+    Flux<MovieInfo> findByYear(Integer year);
+
 }
