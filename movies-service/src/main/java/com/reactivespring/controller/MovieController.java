1d3a25afa17a3951c0956c996b6e56351cb55b49
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-08-18 16:45:43 +0300

    MovieController added with empty getMovieById GET endpoint

--- movies-service/src/main/java/com/reactivespring/controller/MovieController.java
+++ movies-service/src/main/java/com/reactivespring/controller/MovieController.java
@@ -1,12 +1,19 @@
 package com.reactivespring.controller;
 
+import com.reactivespring.domain.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/v1/movie")
 public class MovieController {
 
-
+    @GetMapping("/{id}")
+    public Mono<Movie> getMovieById(@PathVariable String id) {
+        return null;
+    }
 
 }
