63aad37062aa32b6580dd1cf227394030db73567
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-08-18 09:38:15 +0300

    Review added with validation constraints

--- movies-review-service/src/main/java/com/reactivespring/domain/Review.java
+++ movies-review-service/src/main/java/com/reactivespring/domain/Review.java
@@ -1,12 +1,11 @@
 package com.reactivespring.domain;
 
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import javax.validation.constraints.Min;
 
 @Data
@@ -17,8 +16,9 @@ public class Review {
 
     @Id
     private String reviewId;
+    @NotNull(message = "movieInfoId can't be null")
     private Long movieInfoId;
     private String comment;
-    //@Min(value = 0L, message = "rating.negative : rating is negative and please pass a non-negative value")
+    @Min(value = 0L, message = "rating can only be a non-negative value")
     private Double rating;
 }
