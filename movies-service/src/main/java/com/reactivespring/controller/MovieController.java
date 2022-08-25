a3fc46583fd49d8dc754c372e4864c9fa3e727fe
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-08-25 14:54:29 +0300

    MovieController#getMovieById(String id) added with inner logic

--- movies-service/src/main/java/com/reactivespring/controller/MovieController.java
+++ movies-service/src/main/java/com/reactivespring/controller/MovieController.java
@@ -1,6 +1,9 @@
 package com.reactivespring.controller;
 
+import com.reactivespring.client.MoviesInfoRestClient;
+import com.reactivespring.client.ReviewsRestClient;
 import com.reactivespring.domain.Movie;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/v1/movie")
 public class MovieController {
 
+    private final MoviesInfoRestClient moviesInfoRestClient;
+    private final ReviewsRestClient reviewsRestClient;
+
     @GetMapping("/{id}")
     public Mono<Movie> getMovieById(@PathVariable String id) {
-        return null;
+        return moviesInfoRestClient.retrieveMovieInfo(id)
+            .flatMap(movieInfo -> {
+                var reviewsMono = reviewsRestClient.getReviews(id).collectList();
+                return reviewsMono.map(reviews -> new Movie(movieInfo, reviews));
+            });
     }
 
 }
