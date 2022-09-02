fbf15aa60ca7c3b30ea9a4903015aeb3204f7b18
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-09-02 20:14:50 +0300

    GlobalErrorHandler added with unified error-handler for all Runtime exceptions

--- movies-service/src/main/java/com/reactivespring/exception/handler/GlobalErrorHandler.java
+++ movies-service/src/main/java/com/reactivespring/exception/handler/GlobalErrorHandler.java
@@ -2,10 +2,10 @@ package com.reactivespring.exception.handler;
 
 import com.reactivespring.exception.MoviesInfoClientException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.support.WebExchangeBindException;
 
 @Slf4j
 @ControllerAdvice
@@ -13,8 +13,14 @@ public class GlobalErrorHandler {
 
     @ExceptionHandler(MoviesInfoClientException.class)
     public ResponseEntity<String> movieInfoClientExceptionHandler(MoviesInfoClientException exception) {
-        log.error("Error from movie-info-service: ", exception);
+        log.error("Internal error in movie-info-service: ", exception);
         return ResponseEntity.status(exception.getStatusCode()).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> movieInfoServiceExceptionHandler(RuntimeException exception) {
+        log.error("Error from movie-info-service: ", exception);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
     }
 
 }
