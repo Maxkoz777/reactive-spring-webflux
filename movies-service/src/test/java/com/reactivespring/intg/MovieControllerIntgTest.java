cd3a2de823198e2cd3fe797ead079173879b3eca
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-09-02 23:01:18 +0300

    MovieControllerIntgTest added with retrieve movie by id test with the use of WireMock

--- movies-service/src/test/java/com/reactivespring/intg/MovieControllerIntgTest.java
+++ movies-service/src/test/java/com/reactivespring/intg/MovieControllerIntgTest.java
@@ -1,10 +1,16 @@
 package com.reactivespring.intg;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.reactivespring.domain.Movie;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @ActiveProfiles("test")
 @AutoConfigureWireMock(port = 8084)
@@ -16,8 +22,41 @@ import org.springframework.test.context.TestPropertySource;
         "restClient.reviewUrl=http://localhost:8084/v1/review"
     }
 )
-public class MovieControllerIntgTest {
+class MovieControllerIntgTest {
 
+    @Autowired
+    WebTestClient webTestClient;
 
+    @Test
+    void retrieveMovieById() {
+
+        var movieId = "mockId";
+
+        WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/v1/movieInfo/" + movieId))
+                             .willReturn(WireMock.aResponse()
+                                             .withHeader("Content-Type", "application/json")
+                                             .withBodyFile("movieInfo.json")));
+
+        WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/v1/review"))
+                             .willReturn(WireMock.aResponse()
+                                             .withHeader("Content-Type", "application/json")
+                                             .withBodyFile("reviews.json")));
+
+        webTestClient.get()
+            .uri("/v1/movie/{id}", movieId)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody(Movie.class)
+            .consumeWith(movieEntityExchangeResult -> {
+                var movie = movieEntityExchangeResult.getResponseBody();
+                Assertions.assertNotNull(movie, "movie should exist");
+                Assertions.assertEquals(2, movie.getReviewList().size(),
+                                        "There should be 2 reviews for movie entity");
+                Assertions.assertEquals("Batman", movie.getMovieInfo().getName(),
+                                        "Movie name should match the predefined name");
+
+            });
+
+    }
 
 }
