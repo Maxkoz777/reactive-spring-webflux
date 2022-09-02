e2171497ba87e5551687d97f3e89e169412f934b
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-09-02 13:37:44 +0300

    MoviesInfoRestClient added with ability to handle errors from client and show them in response from current microservice

--- movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -1,12 +1,16 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.exception.MoviesInfoClientException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class MoviesInfoRestClient {
@@ -23,6 +27,23 @@ public class MoviesInfoRestClient {
         return webClient.get()
             .uri(url, id)
             .retrieve()
+            .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                    log.error("Error status code: {}", clientResponse.statusCode().value());
+                    return Mono.error(new MoviesInfoClientException(
+                        "There is no movie Info for provided id: " + id,
+                        HttpStatus.NOT_FOUND.value()
+                    ));
+                } else {
+                    return clientResponse.bodyToMono(String.class)
+                        .flatMap(responseMessage -> Mono.error(
+                            new MoviesInfoClientException(
+                                responseMessage,
+                                clientResponse.statusCode().value()
+                            )
+                        ));
+                }
+            })
             .bodyToMono(MovieInfo.class).log();
 
     }
