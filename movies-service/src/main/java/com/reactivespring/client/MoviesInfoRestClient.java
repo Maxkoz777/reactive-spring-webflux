6f677e83d6a84b4b1d92f30ed6b275415ded8386
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-09-02 20:15:44 +0300

    MoviesInfoRestClient movieInfo retrieval added with processing of 5xx status codes

--- movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -2,6 +2,7 @@ package com.reactivespring.client;
 
 import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.exception.MoviesInfoClientException;
+import com.reactivespring.exception.MoviesInfoServerException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -28,8 +29,8 @@ public class MoviesInfoRestClient {
             .uri(url, id)
             .retrieve()
             .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                log.info("Error status code: {}", clientResponse.statusCode().value());
                 if (clientResponse.statusCode() == HttpStatus.NOT_FOUND) {
-                    log.error("Error status code: {}", clientResponse.statusCode().value());
                     return Mono.error(new MoviesInfoClientException(
                         "There is no movie Info for provided id: " + id,
                         HttpStatus.NOT_FOUND.value()
@@ -43,6 +44,13 @@ public class MoviesInfoRestClient {
                             )
                         ));
                 }
+            })
+            .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+                log.info("Status code is: {}", clientResponse.statusCode().value());
+                return clientResponse.bodyToMono(String.class)
+                    .flatMap(message -> Mono.error(
+                        new MoviesInfoServerException("Server exception in movie-info-service " + message))
+                    );
             })
             .bodyToMono(MovieInfo.class).log();
 
