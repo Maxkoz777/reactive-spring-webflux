3f3c617c97b4d56cde91a2e5b630e5a1430f517c
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-09-02 20:21:14 +0300

    ReviewsRestClient updated with actions for 4xx and 5xx status codes from movie-review-service

--- movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
@@ -1,13 +1,21 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.Review;
+import com.reactivespring.exception.MoviesInfoClientException;
+import com.reactivespring.exception.MoviesInfoServerException;
+import com.reactivespring.exception.ReviewsClientException;
+import com.reactivespring.exception.ReviewsServerException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ReviewsRestClient {
@@ -19,6 +27,8 @@ public class ReviewsRestClient {
 
     public Flux<Review> getReviews(String movieId) {
 
+        log.info("Retrieving review by id: {}", movieId);
+
         var url = UriComponentsBuilder.fromHttpUrl(reviewsUrl)
             .queryParam("movieInfoId", movieId)
             .buildAndExpand().toUriString();
@@ -26,6 +36,24 @@ public class ReviewsRestClient {
         return webClient.get()
             .uri(url)
             .retrieve()
+            .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                log.info("Error status code: {}", clientResponse.statusCode().value());
+                if (clientResponse.statusCode() == HttpStatus.NOT_FOUND) {
+                    return Mono.empty();
+                } else {
+                    return clientResponse.bodyToMono(String.class)
+                        .flatMap(responseMessage -> Mono.error(
+                            new ReviewsClientException(responseMessage)
+                        ));
+                }
+            })
+            .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+                log.info("Status code is: {}", clientResponse.statusCode().value());
+                return clientResponse.bodyToMono(String.class)
+                    .flatMap(message -> Mono.error(
+                        new ReviewsServerException("Server exception in movie-review-service " + message))
+                    );
+            })
             .bodyToFlux(Review.class);
 
     }
