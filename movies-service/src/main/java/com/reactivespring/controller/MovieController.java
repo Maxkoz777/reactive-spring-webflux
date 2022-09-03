d963b071aa27fb7c686bb3e38896eec52070fcc0
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-09-03 15:15:16 +0300

    MovieController added with endpoint for SSE for movieInfos addition

--- movies-service/src/main/java/com/reactivespring/controller/MovieController.java
+++ movies-service/src/main/java/com/reactivespring/controller/MovieController.java
@@ -3,11 +3,14 @@ package com.reactivespring.controller;
 import com.reactivespring.client.MoviesInfoRestClient;
 import com.reactivespring.client.ReviewsRestClient;
 import com.reactivespring.domain.Movie;
+import com.reactivespring.domain.MovieInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -25,6 +28,11 @@ public class MovieController {
                 var reviewsMono = reviewsRestClient.getReviews(id).collectList();
                 return reviewsMono.map(reviews -> new Movie(movieInfo, reviews));
             });
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.APPLICATION_NDJSON_VALUE)
+    public Flux<MovieInfo> getMovieServerSentEvents() {
+        return moviesInfoRestClient.retrieveMovieInfoStream();
     }
 
 }
